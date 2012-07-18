2b5ac13f05d8199df386fa9ed4d531675f0d8da0
Mark Nguyen <pnguyen1097@gmail.com>
2012-07-18 11:25:50 -0400

    Add routes to post email and view emails. Ignore database file.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require 'sinatra/base'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+# Email model
+class Email
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, Text, :required => true
+  property :email, String, :required => true
+  property :created_at, DateTime
+end
+
+DataMapper.auto_upgrade!
 
 class Website < Sinatra::Base
 
@@ -21,6 +36,42 @@ class Website < Sinatra::Base
     @page_title = "Welcome to StatRoom"
     @styles = "index"
     erb :index
+  end
+
+  post '/email' do
+    email = Email.new(
+      :name => params[:name],
+      :email => params[:email]
+    )
+
+    if email.save
+      status 201
+      "Thank you, #{params[:name]}. An email will be sent to #{params[:email]} when StatRoom launch."
+    else
+      status 400
+      "An error has occur while saving your email address."
+    end
+  end
+
+  #Listing email for debugging - should be removed before going to prod
+  get '/email' do
+    result = <<RESULT
+<div class='container'>
+<div class='row'>
+<div class='span12'>
+<table class='table table-striped'>
+<thead><tr><th>Name</th><th>Email</th></tr></thead>
+RESULT
+    Email.all.each do |res|
+      result += "<tr><td>"
+      result += res.name + "</td><td>"
+      result += res.email + "</td></tr>"
+    end
+    result += "</table>
+</div>
+</div>
+</div>"
+    erb result
   end
 
 end
