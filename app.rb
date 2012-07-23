3d95b3f604e73ff8dec9329d05e4c391f93d1d11
Mark Nguyen <pnguyen1097@gmail.com>
2012-07-23 10:24:29 -0400

    Finish index page with email submission.

--- app.rb
+++ app.rb
@@ -20,16 +20,19 @@ class Website < Sinatra::Base
   helpers do
     def put_stylesheet(style)
       result = ""
-      if style.is_a?(Array)
-        style.each do |name|
-          result += "<link rel='stylesheet' href='css/#{name}.css' >\n"
+      unless style.nil? or style.empty?
+        if style.is_a?(Array)
+          style.each do |name|
+            result += "<link rel='stylesheet' href='css/#{name}.css' >\n"
+          end
+        else
+          result += "<link rel='stylesheet' href='css/#{style}.css'>\n"
         end
-      else
-        result += "<link rel='stylesheet' href='css/#{style}.css'>\n"
       end
 
       return result;
     end
+
   end
 
   get '/' do
